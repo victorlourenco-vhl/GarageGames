37747f47ae32b9bc005815d96290147875c6fc45
Victor Lourenço <victorhlo12@hotmail.com>
2023-07-24 10:22:08 -0300

    Criando o projeto

---
+++ Garage Games/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Garage_Games.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
