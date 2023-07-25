f1b91a01f9f5e28eb9310a1d3342f399147d8543
Victor Lourenço <victorhlo12@hotmail.com>
2023-07-25 13:22:19 -0300

    FEAT: Iniciando a conexão com a base de dados

---
+++ Garage Games/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Garage_Games.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage_Games.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {    
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<VideoGame> VideoGames { get; set; }
+
+    }
+}
--- Garage Games/Program.cs
+++ Garage Games/Program.cs
@@ -1,7 +1,13 @@
+using Garage_Games.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Setting the Database Connection Context
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
